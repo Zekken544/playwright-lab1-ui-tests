9b7362e341592d79d484e4c576af90fce299a555
Bilguun-Erdene Munkhbat <bilguuneerw@gmail.com>
2026-09-07 11:26:02 +0800

    Codegen login test

---
+++ tests/codegen-example.spec.ts
@@ -0,0 +1,11 @@
+// Codegen
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+});
